3b449df6cdebf43543cd0c82bfbda5b969d861e9
WilliamIsaiasRoque <wroquequi@unsa.edu.pe>
2023-05-17 17:08:50 -0500

    Agregando ejercicio 2 de laboratorio

---
+++ ejer02.java
@@ -0,0 +1,13 @@
+public class ejer02{
+    //Rotacion a la izquierda
+    public static void main (String [] args){
+        int[] arreglo = {1,2,3,4,5};
+        int d=2;
+
+    }
+    public static int[] rotarIzquierda(int[] A, int d){
+        int longitud=A.length;
+        int [] Aiz = new int [longitud];
+        return Aiz;
+    }
+}
