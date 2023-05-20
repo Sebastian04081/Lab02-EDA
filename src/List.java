34948c7c0b278c54ddadade95842b4ca3a193369
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-20 18:24:14 -0500

    Agregando los métodos setters y getters en List.java

--- src/List.java
+++ src/List.java
@@ -1,3 +1,15 @@
 public class List<T> {
-    
+    private Node<T> root;
+
+    public List() {
+        this.root = null;
+    }
+
+    public Node<T> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<T> root) {
+        this.root = root;
+    }
 }
