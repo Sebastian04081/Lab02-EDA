a09a9204cb25a93864ccbc4e892bc1b991f525fa
Geraldine04Umasi <gumasi@unsa.edu.pe>
2023-05-20 21:27:42 -0500

    imprimo el arreglo original y el invertido

--- src/ejer01.java
+++ src/ejer01.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
 
 public class ejer01 {
     public static void main(String[]args){
         int arreglo[]={1,2,3};
         int[] Ain = invertirArray(arreglo);
-        System.out.println("");
+        System.out.println("Arreglo original:\n" + Arrays.toString(arreglo));
+        System.out.println("Arreglo invertido:\n" +Arrays.toString(Ain));
     }
 
     public static int[] invertirArray(int[]A){
