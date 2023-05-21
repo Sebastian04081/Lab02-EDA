84384279688812ef8e3ab3f0153b3abf91508181
William <wroquequi@unsa.edu.pe>
2023-05-21 01:13:55 -0500

    Creacion de cantidadRotaciones para evitar que las posiciones a rotar no excedan el tamaño del arreglo

--- src/Ejercicio02.java
+++ src/Ejercicio02.java
@@ -1,15 +1,17 @@
+import java.util.Arrays;
 public class Ejercicio02 {
     //Rotacion a la izquierda
     public static void main (String [] args){
         int[] A = {1,2,3,4,5};
         int d=2;
-        //Método rotarIzquierda
-
+        immprimirArray(A);
 
     }
     public static int[] rotarIzquierdaArray(int[] A, int d){
-        int longitud=A.length;
-        int [] Aiz = new int [longitud];
+        int longitud = A.length;
+        int[] Aiz = new int[longitud];
+        int cantidadRotaciones = d % longitud;
+        ///
         return Aiz;
     }
     public static void imprimirArray(int[] A){
