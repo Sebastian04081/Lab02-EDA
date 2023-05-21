81dddca9b9c5e4aca2ecd73061a9dcc6fcff04c5
William <wroquequi@unsa.edu.pe>
2023-05-21 07:04:02 -0500

    Se modificó el main para que imprima los arreglos

--- src/Ejercicio02.java
+++ src/Ejercicio02.java
@@ -1,11 +1,13 @@
-import java.util.Arrays;
 public class Ejercicio02 {
-    //Rotacion a la izquierda
     public static void main (String [] args){
         int[] A = {1,2,3,4,5};
-        int d=2; 
-        immprimirArray(A);
-
+        int d=2;
+        int[] Aiz = rotarIzquierdaArray(A,d);
+        System.out.print("A=[");
+        imprimirArray(A);
+        System.out.print("] -> Aiz=[");
+        imprimirArray(Aiz);
+        System.out.print("]");
     }
     public static int[] rotarIzquierdaArray(int[] A, int d){
         int longitud = A.length;
@@ -13,7 +15,7 @@ public class Ejercicio02 {
         int cantidadRotaciones = d % longitud;
         for (int i=0;i<longitud;i++){
             int aux = (i+cantidadRotaciones)%longitud;
-            Aiz|[i] = A[aux];
+            Aiz[i] = A[aux];
         }
         return Aiz;
     }
