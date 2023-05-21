10bb9e68856d686e45123be0ca0e5442e072c11e
Geraldine04Umasi <gumasi@unsa.edu.pe>
2023-05-20 21:21:27 -0500

    Le doy funcionalidad al metodo invertirArray

--- src/ejer01.java
+++ src/ejer01.java
@@ -1,17 +1,16 @@
-import java.util.*;
 
 public class ejer01 {
-    /**
-     * @param args
-     */
     public static void main(String[]args){
-        int arreglo[]={1,2,3,4,5};
+        int arreglo[]={1,2,3};
         
         System.out.println("");
     }
 
-    public int[] invertirArray(int[]A){
-        int[] Ain = {};
+    public static int[] invertirArray(int[]A){
+        int[] Ain = new int [A.length];
+        for(int i=0; i<A.length; i++){
+            Ain[i]= A[A.length-1-i];
+        }
         return Ain;
     }
 }
