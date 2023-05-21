f2d3974f8e3d3f20c4cd7007811271758c37d29b
William <wroquequi@unsa.edu.pe>
2023-05-21 06:46:57 -0500

    Creacion del método rotarIzquierdaArray y una variable auxiliar

--- src/Ejercicio02.java
+++ src/Ejercicio02.java
@@ -3,7 +3,7 @@ public class Ejercicio02 {
     //Rotacion a la izquierda
     public static void main (String [] args){
         int[] A = {1,2,3,4,5};
-        int d=2;
+        int d=2; 
         immprimirArray(A);
 
     }
@@ -11,7 +11,10 @@ public class Ejercicio02 {
         int longitud = A.length;
         int[] Aiz = new int[longitud];
         int cantidadRotaciones = d % longitud;
-        ///
+        for (int i=0;i<longitud;i++){
+            int aux = (i+cantidadRotaciones)%longitud;
+            Aiz|[i] = A[aux];
+        }
         return Aiz;
     }
     public static void imprimirArray(int[] A){
