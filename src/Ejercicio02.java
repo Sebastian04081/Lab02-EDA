4689c16e5122fbf758689de20b3080b6a2266004
William <wroquequi@unsa.edu.pe>
2023-05-20 23:49:05 -0500

    Creacion del método imprimirArray

--- src/Ejercicio02.java
+++ src/Ejercicio02.java
@@ -1,13 +1,20 @@
-public class ejer02 {
+public class Ejercicio02 {
     //Rotacion a la izquierda
     public static void main (String [] args){
-        int[] arreglo = {1,2,3,4,5};
+        int[] A = {1,2,3,4,5};
         int d=2;
+        //Método rotarIzquierda
+
 
     }
-    public static int[] rotarIzquierda(int[] A, int d){
+    public static int[] rotarIzquierdaArray(int[] A, int d){
         int longitud=A.length;
         int [] Aiz = new int [longitud];
         return Aiz;
+    }
+    public static void imprimirArray(int[] A){
+        for (int i=0;i<A.length;i++){
+            System.out.print(A[i]+" ");
+        }
     }
 }
